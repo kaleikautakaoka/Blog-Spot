34f64bcaefcb539ebccae9a0f6d2527f73672927
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-07-23 21:52:34 -0600

    models, associations for users and their posts

--- models/index.js
+++ models/index.js
@@ -3,5 +3,37 @@ const Post = require('./post');
 const View = require('./view');
 
 
-//Associating the tables with belongsTo and hasMany
-User.hasMany(
+//Adding associations for the user and post
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//belongs to user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//belongs to post
+View.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+//belongs to user
+View.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//belongs to someone else's post
+Post.hasMany(View, {
+    foreignKey: 'post_id'
+});
+
+//belongs to someone else's user
+User.hasMany(View, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Post, View };
