12af260a0eb150a495c683bfebff2370395c96b1
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-07-23 22:23:46 -0600

    user route

---
+++ controllers/api/user-route.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+//router for posting for new user
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        }
+        );
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+);
+
+//router for logging in
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { username: req.body.username } });
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You are now logged in!' });
+        }
+        );
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+);
+
+//router for logging out
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        }
+        );
+    } else {
+        res.status(404).end();
+    }
+}
+);
+
+//module export
+module.exports = router;
