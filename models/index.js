ee77b2a4c336a73925b1e8bddc9f56cf3d0c9350
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-07-23 21:27:39 -0600

    table and coloum models

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./user');
+const Post = require('./post');
+const View = require('./view');
+
+
+//Associating the tables with belongsTo and hasMany
+User.hasMany(
---
+++ models/view.js
@@ -0,0 +1,45 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class View extends Model {}
+
+//defining table columns and configurations
+
+View.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'view'
+    }
+);
+
+//model exports
+module.exports = View;
