39cb3805df141237fc538c9056d125b0e9b3e31c
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-07-23 22:55:13 -0600

    formatted date and adding profile js in public

---
+++ utils/helpers.js
@@ -0,0 +1,8 @@
+//helper for formatting date
+module.exports = {
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    }
+};
+
+
