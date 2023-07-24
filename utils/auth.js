88e40cd967e4c22706464eb1efdeb6962c67c978
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-07-23 20:15:05 -0600

    authentication added to utils

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// Ensures that a user is logged in before allowing them to access certain routes
+
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+module.exports = withAuth;
