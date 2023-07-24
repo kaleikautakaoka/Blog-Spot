b2713435bb84cb634456ef4b388fd0bc6a9cdcbf
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-07-23 22:48:43 -0600

    post data seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,4 +1,25 @@
 const sequelize = require('../config/connection');
 const { User, Post, View } = require('../models');
 
-const userData = require('./data.json');
+const starterData = require('./data.json');
+const userData = require('./user.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const post of starterData) {
+        await Post.create({
+            ...starter,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
+
+    process.exit(0);
+}
+
+
