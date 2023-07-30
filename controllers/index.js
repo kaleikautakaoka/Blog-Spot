5c5274b6ff9032295b4b089cb6105b97506a7758
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-07-30 16:23:57 -0600

    added the homeroutes

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 
 //const all api routes
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 //router.use('/api', apiroutes);
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 //module.exports = router;
 module.exports = router;
