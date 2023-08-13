98058f7acc3ddebc448de79b7751bb37ae68fb72
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-08-12 22:24:09 -0600

    view typo

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 //get route to join post and user tables
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, View } = require('../models');
 // const withAuth = require('../utils/auth');
 
 //get route to join post and user tables
